1e313e3e1ef5e74eae9c94415400e7fda6ddd614
AlexAR <moltron_nano@hotmail.com>
2023-02-01 15:50:26 -0600

    feat: implicits and helper methods

--- src/main/scala/ChannelTypeClass.scala
+++ src/main/scala/ChannelTypeClass.scala
@@ -8,12 +8,12 @@ trait ByteTypeClassEncoder [A] {
 }
 
 trait ChannelByteTypeClass {
-  def write[A](obj: A, encoder: ByteTypeClassEncoder[A]) : Unit
+  def write[A](obj: A) (implicit encoder: ByteTypeClassEncoder[A]) : Unit
 }
 
 object FileChannelByTypeClass extends ChannelByteTypeClass {
 
-  override def write[A](obj: A, encoder: ByteTypeClassEncoder[A]): Unit = {
+  override def write[A](obj: A)(implicit encoder: ByteTypeClassEncoder[A]): Unit = {
 
     val bytes: Array[Byte] = encoder.encode(obj)
 
@@ -24,25 +24,61 @@ object FileChannelByTypeClass extends ChannelByteTypeClass {
   }
 }
 
+case class Switch(isOn: Boolean)
+//can also be provided in the companion object
+object Switch {
+  implicit object SwitchEncoder extends ByteTypeClassEncoder[Switch] {
+    override def encode(a: Switch): Array[Byte] = {
+      a.isOn match {
+        case a: Boolean if a == true => Array('1'.toByte)
+        case a: Boolean if a == false => Array('0'.toByte)
+      }
+    }
+  }
+}
+
 //define different encoders:
-object IntByteEncoder extends ByteTypeClassEncoder[Int] {
-  override def encode(a: Int): Array[Byte] = {
-    val bb = ByteBuffer.allocate(4)
-    bb.putInt(a)
-    bb.array()
-  }
-}
+object MainEncoder {
 
-object StringByteEncoder extends ByteTypeClassEncoder[String] {
-  override def encode(a: String): Array[Byte] = a.getBytes()
-}
 
-object FullNameByteEncoder extends ByteTypeClassEncoder[FullName] {
-  override def encode(a: FullName): Array[Byte] = {
-    s"${a.firstName} ${a.lastName}".getBytes()
+  implicit object IntByteEncoder extends ByteTypeClassEncoder[Int] {
+    override def encode(a: Int): Array[Byte] = {
+      val bb = ByteBuffer.allocate(4)
+      bb.putInt(a)
+      bb.array()
+    }
   }
+
+  implicit object StringByteEncoder extends ByteTypeClassEncoder[String] {
+    override def encode(a: String): Array[Byte] = a.getBytes()
+  }
+
+  implicit object FullNameByteEncoder extends ByteTypeClassEncoder[FullName] {
+    override def encode(a: FullName): Array[Byte] = {
+      s"${a.firstName} ${a.lastName}".getBytes()
+    }
+  }
+
+  implicit val floatEncoder : ByteTypeClassEncoder[Float] = new ByteTypeClassEncoder[Float] {
+    override def encode(a: Float): Array[Byte] = Array(a.toByte)
+  }
+
+  def instance[A](f: A => Array[Byte]): ByteTypeClassEncoder[A] = {
+    new ByteTypeClassEncoder[A] {
+      override def encode(a: A): Array[Byte] = f(a)
+    }
+  }
+
+  implicit val doubleEncoder = instance[Double](d => Array(d.toByte))
+
+  def apply[A](implicit ev: ByteTypeClassEncoder[A]): ByteTypeClassEncoder[A] = ev
+
 }
 
 object ChannelTypeClassApp extends App {
-  FileChannelByTypeClass.write(FullName("Cersei", "Lannister"), FullNameByteEncoder)
+  import MainEncoder._
+  //FileChannelByTypeClass.write(FullName("Cersei", "Lannister2"))
+  //FileChannelByTypeClass.write(Switch(true))
+  FileChannelByTypeClass.write(1921.312)
+
 }
